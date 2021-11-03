fdf2a9a3062d3204c48fcf1b1d2099f02318dfa7
Benedikt Schuh - A19 <benedikt.schuh@nordakademie.de>
2021-11-03 13:06:27 +0100

    Added implementation  of LogicalOR

--- src/main/java/circuitcomponents/LogicalOR.java
+++ src/main/java/circuitcomponents/LogicalOR.java
@@ -2,9 +2,18 @@ package circuitcomponents;
 
 import exceptions.MissingInputException;
 
+import java.util.List;
+
 public class LogicalOR extends LogicalComponent{
     @Override
     public void calculateState() throws MissingInputException {
+        List<Component> inputs = getInputs();
+        if (inputs.isEmpty()) throw new MissingInputException();
 
+        boolean result = false;
+        for (Component input:inputs) {
+            result = result || input.getState();
+        }
+        setState(result);
     }
 }
