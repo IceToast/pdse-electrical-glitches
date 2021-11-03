4d3b70b5b41f8ebd74f07b8d2698ccf897c83195
Benedikt Schuh - A19 <benedikt.schuh@nordakademie.de>
2021-11-03 12:55:53 +0100

    Added tests LogicalORTest with LogicalOR(no implementation)

---
+++ src/main/java/circuitcomponents/LogicalOR.java
@@ -0,0 +1,10 @@
+package circuitcomponents;
+
+import exceptions.MissingInputException;
+
+public class LogicalOR extends LogicalComponent{
+    @Override
+    public void calculateState() throws MissingInputException {
+
+    }
+}
---
+++ src/test/java/circuitcomponents/LogicalORTest.java
@@ -0,0 +1,85 @@
+package circuitcomponents;
+
+import exceptions.MissingInputException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class LogicalORTest {
+    private LogicalOR logicalOR;
+    private Component logicalInput1;
+    private Component logicalInput2;
+
+    @Before
+    public void setUp(){
+        logicalOR = new LogicalOR();
+        logicalInput1 = mock(LogicalInput.class);
+        logicalInput2 = mock(LogicalInput.class);
+    }
+
+    @Test
+    public void calculateStateWithOneInputF() {
+        when(logicalInput1.getState()).thenReturn(false);
+        logicalOR.addInput(logicalInput1);
+
+        logicalOR.calculateState();
+
+        assertFalse(logicalOR.getState());
+    }
+
+    @Test
+    public void calculateStateWithOneInputT() {
+        when(logicalInput1.getState()).thenReturn(true);
+        logicalOR.addInput(logicalInput1);
+
+        logicalOR.calculateState();
+
+        assertTrue(logicalOR.getState());
+    }
+
+    @Test
+    public void calculateStateWithTwoInputsFF() {
+        when(logicalInput1.getState()).thenReturn(false);
+        when(logicalInput2.getState()).thenReturn(false);
+        logicalOR.addInput(logicalInput1);
+        logicalOR.addInput(logicalInput2);
+
+        logicalOR.calculateState();
+
+        assertFalse(logicalOR.getState());
+    }
+
+    @Test
+    public void calculateStateWithTwoInputsFT() {
+        when(logicalInput1.getState()).thenReturn(false);
+        when(logicalInput2.getState()).thenReturn(true);
+        logicalOR.addInput(logicalInput1);
+        logicalOR.addInput(logicalInput2);
+
+        logicalOR.calculateState();
+
+        assertTrue(logicalOR.getState());
+    }
+
+    @Test
+    public void calculateStateWithTwoInputsTT() {
+        when(logicalInput1.getState()).thenReturn(true);
+        when(logicalInput2.getState()).thenReturn(true);
+        logicalOR.addInput(logicalInput1);
+        logicalOR.addInput(logicalInput2);
+
+        logicalOR.calculateState();
+
+        assertTrue(logicalOR.getState());
+    }
+
+    @Test(expected = MissingInputException.class)
+    public void calculateStateWithNoInput() {
+        logicalOR.calculateState();
+
+        assertThrows(MissingInputException.class, () -> logicalOR.calculateState());
+    }
+}
