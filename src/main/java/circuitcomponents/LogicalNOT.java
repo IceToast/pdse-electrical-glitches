ab4f3a83185f502becc0377195819c9654f96bf5
Mattes Witthöft - A19 <mattes.witthoeft@nordakademie.de>
2021-11-03 21:42:00 +0100

    Added implementation of LogicalNOT

--- src/main/java/circuitcomponents/LogicalNOT.java
+++ src/main/java/circuitcomponents/LogicalNOT.java
@@ -1,12 +1,34 @@
 package circuitcomponents;
 
+import exceptions.MissingInputException;
+import exceptions.TooManyInputsException;
+
+import java.util.Collections;
+import java.util.List;
+
 public class LogicalNOT extends LogicalComponent {
 
     @Override
     public void calculateState() {
+        List<Component> inputs = getInputs();
+        if (inputs.isEmpty()) throw new MissingInputException();
+
+        Component input = inputs.get(0);
+        boolean result = !input.getState();
+        setState(result);
+        callCalculateOnChildren();
     }
 
     @Override
     public void addInput(Component... component) {
+        List<Component> inputs = getInputs();
+        if(component.length <= 0)
+            throw new MissingInputException("No Input to add");
+
+        if(inputs.size() == 1 || component.length > 1)
+            throw new TooManyInputsException("A LogicalNOT can only handle 1 input");
+
+
+        Collections.addAll(getInputs(), component);
     }
 }
