4153ce515e64c4d632dce7a66432f31ca58b550a
Mattes Witthöft - A19 <mattes.witthoeft@nordakademie.de>
2021-11-03 09:57:21 +0100

    Added test for method calculateState() to test one input with false- mit Maximilian Glöe

--- src/test/java/circuitcomponents/LogicalANDTest.java
+++ src/test/java/circuitcomponents/LogicalANDTest.java
@@ -4,14 +4,19 @@ import exceptions.MissingInputException;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class LogicalANDTest {
     private LogicalAND logicalAND;
+    private Component logicalInput1;
 
     @Before
     public void setUp() {
         logicalAND = new LogicalAND();
+        logicalInput1 = mock(LogicalInput.class);
     }
 
     @Test(expected = MissingInputException.class)
@@ -19,5 +24,15 @@ public class LogicalANDTest {
         logicalAND.calculateState();
 
         assertThrows(MissingInputException.class, () -> logicalAND.calculateState());
+    }
+
+    @Test
+    public void calculateStateWithOneInputF() {
+        when(logicalInput1.getState()).thenReturn(false);
+        logicalAND.addInput(logicalInput1);
+
+        logicalAND.calculateState();
+
+        assertFalse(logicalAND.getState());
     }
 }
