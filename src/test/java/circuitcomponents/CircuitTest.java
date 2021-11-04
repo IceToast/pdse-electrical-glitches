ac1b16ae9d77f8c877c44b0e949dd5eb30361236
Timo Dohl - A19 <timo.dohl@nordakademie.de>
2021-11-04 18:47:17 +0100

    Added another calculateMaxToggleTime test in cicruitTest

--- src/test/java/circuitcomponents/CircuitTest.java
+++ src/test/java/circuitcomponents/CircuitTest.java
@@ -31,10 +31,31 @@ public class CircuitTest {
     public void calculateMaxToggleTimeWithOneLogicalComponent() {
         when(inputX1.getChannel()).thenReturn(1);
         when(inputX2.getChannel()).thenReturn(2);
+
         logicalOR.addInput(inputX1, inputX2);
+
         when(logicalOR.getInputs()).thenReturn(Arrays.asList(new Component[]{inputX1, inputX2}));
+
         circuit = new Circuit(logicalOR);
+        
         when(logicalOR.calculateToggleTime()).thenAnswer(I -> Math.max(1 + inputX1.calculateToggleTime(), 1 + inputX2.calculateToggleTime()));
         assertEquals(1, circuit.calculateMaxToggleTime());
+    }
+
+    @Test
+    public void calculateMaxToggleTimeWithTwoLogicalComponentsInARow() {
+        when(inputX1.getChannel()).thenReturn(1);
+        when(inputX2.getChannel()).thenReturn(2);
+
+        logicalNOT.addInput(inputX1);
+        logicalOR.addInput(inputX2, logicalNOT);
+
+        when(logicalNOT.getInputs()).thenReturn(Arrays.asList(new Component[]{inputX1}));
+        when(logicalOR.getInputs()).thenReturn(Arrays.asList(new Component[]{inputX2, logicalNOT}));
+
+        circuit = new Circuit(logicalOR);
+
+        when(logicalOR.calculateToggleTime()).thenAnswer(I -> Math.max(1 + inputX2.calculateToggleTime(), 1 + logicalNOT.calculateToggleTime()));
+        assertEquals(2, circuit.calculateMaxToggleTime());
     }
 }
