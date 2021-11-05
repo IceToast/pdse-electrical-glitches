136b34f1ec31df115de6bf1fc3d4de8c6607fadf
Mattes Witthöft - A19 <mattes.witthoeft@nordakademie.de>
2021-11-05 16:43:42 +0100

    Implements a method that removes all duplicate inputs in a list of inputs

--- src/main/java/circuitcomponents/Circuit.java
+++ src/main/java/circuitcomponents/Circuit.java
@@ -3,10 +3,7 @@ package circuitcomponents;
 import exceptions.DuplicateInputChannelDetectedException;
 import exceptions.TopComponentNoInputsSetException;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -25,6 +22,14 @@ public class Circuit {
         if(this.topComponent.getInputs().size() <= 0){
             throw new TopComponentNoInputsSetException();
         }
+    }
+
+    private List<LogicalInput> removeDuplicatesFromList(List<LogicalInput> usedInputs) {
+        Set<LogicalInput> duplicateCheck = new HashSet<>(usedInputs);
+        usedInputs = new ArrayList<>();
+        usedInputs.addAll(duplicateCheck);
+        sortInputsByChannel(usedInputs);
+        return usedInputs;
     }
 
     private void sortInputsByChannel(List<LogicalInput> usedInputs) {
