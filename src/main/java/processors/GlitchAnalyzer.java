ecd113bf0291295067f8c361391a6a266b34e845
Benedikt Schuh - A19 <benedikt.schuh@nordakademie.de>
2021-11-06 12:01:58 +0100

    Added implementation for GlitchAnalyzer.analyzeForGlitches and private method GlitchAnalyzer.analyzeGlitchesPerInputSet

--- src/main/java/processors/GlitchAnalyzer.java
+++ src/main/java/processors/GlitchAnalyzer.java
@@ -15,6 +15,15 @@ public class GlitchAnalyzer {
     }
 
     public HashMap<Integer, List<Boolean>> analyzeForGlitches() {
+        HashMap<Integer, List<Boolean>> glitches = new HashMap<>();
+        for (int i = 0; i < Math.pow(2, inputs.size()); i++) {
+            glitches.put(i, analyzeGlitchesPerInputSet(i));
+        }
+
         return new HashMap<Integer, List<Boolean>>();
+    }
+
+    private List<Boolean> analyzeGlitchesPerInputSet(int i) {
+        return null;
     }
 }
