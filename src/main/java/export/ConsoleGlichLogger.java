ce14d496b71c5d242458a2aa60d88b58d04db3c7
Mattes Witthöft - A19 <mattes.witthoeft@nordakademie.de>
2021-11-07 13:40:24 +0100

    Added class ConsoleGlichLogger with method log(). No implementation of log()

---
+++ src/main/java/export/ConsoleGlichLogger.java
@@ -0,0 +1,9 @@
+package export;
+
+import java.util.List;
+import java.util.Map;
+
+public class ConsoleGlichLogger implements GlitchLogger {
+    public void log(Map<Integer, List<Boolean>> glitches) {
+    }
+}
