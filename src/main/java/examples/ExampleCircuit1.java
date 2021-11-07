d415e050770c660f2f72bb5e707ef13fcfe4d0b7
Maximilian Glöe - A19 <maximilian.gloee@nordakademie.de>
2021-11-07 13:59:54 +0100

    Example1 from Task added

---
+++ src/main/java/examples/ExampleCircuit1.java
@@ -0,0 +1,23 @@
+package examples;
+
+import circuitcomponents.*;
+
+public class ExampleCircuit1 implements ExampleCircuit{
+    public Circuit create(){
+        LogicalInput inputX0 = new LogicalInput(0);
+        LogicalInput inputX1 = new LogicalInput(1);
+        LogicalInput inputX2 = new LogicalInput(2);
+
+        LogicalAND a1 = new LogicalAND();
+        LogicalAND a2 = new LogicalAND();
+        LogicalNOT n1 = new LogicalNOT();
+        LogicalOR o1 = new LogicalOR();
+
+        o1.addInput(a1, a2);
+        a1.addInput(inputX0, inputX1);
+        a2.addInput(n1, inputX2);
+        n1.addInput(inputX1);
+
+        return new Circuit(o1);
+    }
+}
